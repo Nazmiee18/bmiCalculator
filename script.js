a63d4bd15302b27ad51092380ec4a0887684a95b
Nazmiee18 <mfirdaus.nz1819@gmail.com>
2023-10-06 22:41:00 +0700

    give JavaScript

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const btnCalculator = document.getElementById('btncal');
+  const inputWeight = document.getElementById('weight');
+  const inputHeight = document.getElementById('height');
+  const hasilBmi = document.getElementById('hasil');
+
+  btnCalculator.addEventListener('click', () => {
+    const berat = parseFloat(inputWeight.value);
+    const tinggi = parseFloat(inputHeight.value);
+    const bmiCal = berat / (tinggi / 100) ** 2;
+    const bulatBmi = bmiCal.toFixed(1);
+    let result = '';
+
+    if (!isNaN(bulatBmi)) {
+      if (bulatBmi <= 18.5) {
+        result = 'Underweight';
+      } else if (bulatBmi <= 24.9) {
+        result = 'Normalweight';
+      } else if (bulatBmi <= 29.9) {
+        result = 'Overweight';
+      } else {
+        result = 'Obesity';
+      }
+      hasilBmi.innerText = `Your BMI is ${bulatBmi} which means You are ${result}`;
+    } else {
+      hasilBmi.innerText = 'The number you entered is invalid';
+    }
+  });
+});
